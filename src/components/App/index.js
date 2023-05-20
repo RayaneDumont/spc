625f4cf9d0123be0c25c46952a82650b875c78bf
RayaneDumont <rayane.dumont@gmail.com>
2023-05-20 19:28:15 +0200

    adding preload auto to audio

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -99,7 +99,7 @@ function App() {
 
   return (
     <div className="App">
-      <audio ref={soundRef} src={selectedSound} preload="none" />
+      <audio ref={soundRef} src={selectedSound} preload="auto" />
       <header className="App-header">
         <img className='logo-spc' src={logo} ></img>
         <p className='social-medias'>
